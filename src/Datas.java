75c90a0b50f68309a08b193f0159b6189bfc101e
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2021-10-29 22:01:19 -0300

    Utilizando a nova API de Datas

---
+++ src/Datas.java
@@ -0,0 +1,31 @@
+import java.text.DateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class Datas {
+    public static void main(String[] args) {
+        LocalDate hoje = LocalDate.now();
+        System.out.println(hoje);
+
+        LocalDate olimpiadasToquio = LocalDate.of(2021, Month.JUNE, 5);
+
+        int anos = olimpiadasToquio.getYear() - hoje.getYear();
+
+        Period periodo = Period.between(hoje, olimpiadasToquio);
+
+        LocalDate proximasOlimpiadas = olimpiadasToquio.plusYears(4);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataFormatada = proximasOlimpiadas.format(formatador);
+
+        System.out.println(dataFormatada);
+
+        DateTimeFormatter formatadorComHoras = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm");
+
+        LocalDateTime agora = LocalDateTime.now();
+        System.out.println(agora.format(formatadorComHoras));
+
+        LocalTime intervalo = LocalTime.of(15, 30);
+        System.out.println(intervalo);
+    }
+}
